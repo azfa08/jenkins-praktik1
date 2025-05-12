b91100b6145bfdcd0bc084cda2584a3eee66927e
azfa08 <116727708+azfa08@users.noreply.github.com>
2025-05-12 20:02:49 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,5 @@
 from app import app
 
-def test_hello():
-    client = app.test_client()
-    response = client.get('/')
+def test_home():
+    response = app.test_client().get("/")
     assert response.status_code == 200
-    assert b"Hello from Jenkins Multibranch Pipeline!" in response.data
