ecf03260d71947fc656c89fe4175973309166d00
azfa08 <116727708+azfa08@users.noreply.github.com>
2025-05-12 20:02:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from app import app
+from flask import Flask
 
-def test_hello():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Hello from Jenkins Multibranch Pipeline!" in response.data
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
